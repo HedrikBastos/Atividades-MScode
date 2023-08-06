3361e8cf84a94f627be2d0b4b8f444907d8e4fab
Hedrik Bastos <89878358+HedrikBastos@users.noreply.github.com>
2023-08-06 17:01:49 -0300

    Update ConsultaProdutos.php

--- 25-07/05-Consulta-Tabela-Produtos/ConsultaProdutos.php
+++ 25-07/05-Consulta-Tabela-Produtos/ConsultaProdutos.php
@@ -6,7 +6,7 @@ class ConsultaProduto
     public function consultaProdutos()
     {
         
-        $resultado= MinhaMysqli::conectar()->query("SELECT `idproduto`, `titulo`, `pagamento`, `valor` FROM `produtos`");
+        $resultado= MinhaMysqli::conectar()->query("SELECT `id`, `titulo`, `pagamento`, `valor` FROM `produtos`");
 
         return mysqli_fetch_all($resultado);
     }
