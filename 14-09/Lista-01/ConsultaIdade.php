b7e6755010bdf2dde0a43ac0efb5c12be0adbbd3
Hedrik Bastos <89878358+HedrikBastos@users.noreply.github.com>
2023-09-14 23:47:54 -0300

    Update

--- 14-09/Lista-01/ConsultaIdade.php
+++ 14-09/Lista-01/ConsultaIdade.php
@@ -11,10 +11,3 @@ class ConsultaClientes
         return mysqli_fetch_all($resultado, MYSQLI_ASSOC);
     }
 }
-
-$teste = new ConsultaClientes();
-$clientes = $teste->consultaIdade();
-
-foreach ($clientes as $clientes) {
-    echo "Nome: " . $clientes['nome'] . " " . $clientes['sobrenome'] . ", idade: " . $clientes['idade'] . "<br>";
-}
